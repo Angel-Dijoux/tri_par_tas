b4be21b0fc012f78032ca8681eb980f0c2bc4c3a
Angel Dijoux <angel.dijoux@yahoo.com>
2023-04-03 00:39:08 +0200

    Trying haskell

--- haskell/Headsort.hs
+++ haskell/Headsort.hs
@@ -1,9 +1,16 @@
-module Headsort (
-    Headsort(..),
-    droite,
-) where
+module Headsort
+  ( Headsort (..),
+    trierParTas,
+    trierListe,
+    maximune,
+    entasser,
+  )
+where
 
-data Headsort = Headsort { liste :: [Int] }
+import Control.Concurrent.STM (check)
+import Text.XHtml (lime)
+
+newtype Headsort = Headsort {liste :: [Int]}
 
 gauche :: Int -> Int
 gauche i = i * 2 + 1
@@ -13,3 +20,39 @@ droite i = i * 2 + 2
 
 pere :: Int -> Int
 pere i = (i - 1) `div` 2
+
+estTas :: Headsort -> Bool
+estTas hs = all (\i -> liste hs !! gauche i <= liste hs !! i && (droite i >= length (liste hs) || liste hs !! droite i <= liste hs !! i)) [0 .. ((length (liste hs) - 2) `div` 2)]
+
+maximune :: Headsort -> Int -> Int -> Maybe Int
+maximune hs i lim
+  | g < lim && liste hs !! g > liste hs !! maxi = Just g
+  | d < lim && liste hs !! d > liste hs !! maxi = Just d
+  | otherwise = Nothing
+  where
+    maxi = i
+    g = gauche i
+    d = droite i
+
+entasser :: Headsort -> Int -> Int -> Headsort
+entasser hs i lim
+  | maxi /= i = entasser (Headsort new_liste) maxi lim
+  | otherwise = hs
+  where
+    Just maxi = maximune hs i lim
+    temp = liste hs !! i
+    new_liste = take i (liste hs) ++ [liste hs !! maxi] ++ drop (i + 1) (take len (liste hs)) ++ [temp] ++ drop (lim + 1) (liste hs)
+    len = length (liste hs)
+
+buildheap :: Headsort -> Headsort
+buildheap hs = foldr (\i acc -> entasser acc i (length (liste hs))) hs [length (liste hs) - 1, length (liste hs) - 2 .. 0]
+
+trierParTas :: Headsort -> [Int]
+trierParTas hs = buildheap hs' `seq` foldr (\i acc -> let new_hs = entasser (Headsort (liste hs)) 0 i in new_hs `seq` acc ++ [head (liste new_hs)]) [] [(length (liste hs') - 1), (length (liste hs') - 2) .. 1]
+  where
+    hs' = Headsort (reverse (liste hs))
+
+trierListe :: [Int] -> [Int]
+trierListe xs = trierParTas hs
+  where
+    hs = Headsort xs
--- haskell/main.hs
+++ haskell/main.hs
@@ -1,7 +1,7 @@
 import Headsort
 
-main :: IO()
-main = do
-    let myObj = Headsort { liste = [3,4,6,1] }
-    let droite2 = test 2
-    print droite2 
+main :: IO ()
+main =
+  do
+    let hs = Headsort [3, 4, 6, 1, 5, 6, 7, 7]
+    print (maximune hs 0 7)
